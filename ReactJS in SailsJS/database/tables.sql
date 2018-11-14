eec3e072a2ab760d7eba347af3261aa368e66389
gerardojulien <45019193+gerardojulien@users.noreply.github.com>
2018-11-14 11:25:15 -0600

    Add files via upload

---
+++ ReactJS in SailsJS/database/tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Cars (
+    CarId CHAR(36) NOT NULL PRIMARY KEY, 
+    CarName VARCHAR(50), 
+    CarDescription VARCHAR(200), 
+    CarImageUrl VARCHAR(800), 
+    CarCreatedDate DATETIME
+    );  
